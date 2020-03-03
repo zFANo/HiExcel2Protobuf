80e139d978eb1bb23e804492396d78e384bafa6a
zFANo <zf-ano@163.com>
2020-03-04 01:16:24 +0800

    添加导出日志

--- src/HiProtobuf.Lib/LanguageGenerater.cs
+++ src/HiProtobuf.Lib/LanguageGenerater.cs
@@ -5,6 +5,7 @@
  * Author: hiramtan@live.com
  ****************************************************************************/
 using System.IO;
+using HiFramework.Log;
 
 namespace HiProtobuf.Lib
 {
@@ -21,11 +22,21 @@ namespace HiProtobuf.Lib
             Directory.CreateDirectory(_languageFolder);
 
             var protoFolder = Settings.Export_Folder + Settings.proto_folder;
+            Log.Info("开始导出cs");
             Process_csharp(protoFolder);
+            Log.Info("导出cs文件完毕");
+            Log.Info("开始导出cpp");
             Process_cpp(protoFolder);
+            Log.Info("导出cpp文件完毕");
+            Log.Info("开始导出golang文件");
             Process_go(protoFolder);
+            Log.Info("导出golang文件完毕");
+            Log.Info("开始导出java文件");
             Process_java(protoFolder);
+            Log.Info("导出java文件完毕");
+            Log.Info("开始导出python文件");
             Process_python(protoFolder);
+            Log.Info("导出python文件完毕");
         }
 
         private void Process_csharp(string protoPath)
@@ -37,6 +48,8 @@ namespace HiProtobuf.Lib
             for (int i = 0; i < files.Length; i++)
             {
                 var filePath = files[i];
+                string protoName = Path.GetFileNameWithoutExtension(filePath);
+                Log.Info($"{protoName} proto ==> cs");
                 var command = Settings.Protoc_Path + string.Format(" -I={0} --csharp_out={1} {2}", protoPath, outFolder, filePath);
                 var log = Common.Cmd(command);
             }
@@ -51,6 +64,8 @@ namespace HiProtobuf.Lib
             for (int i = 0; i < files.Length; i++)
             {
                 var filePath = files[i];
+                string protoName = Path.GetFileNameWithoutExtension(filePath);
+                Log.Info($"{protoName} proto ==> cpp");
                 var command = Settings.Protoc_Path + string.Format(" -I={0} --cpp_out={1} {2}", protoPath, outFolder, filePath);
                 var log = Common.Cmd(command);
             }
@@ -65,6 +80,8 @@ namespace HiProtobuf.Lib
             for (int i = 0; i < files.Length; i++)
             {
                 var filePath = files[i];
+                string protoName = Path.GetFileNameWithoutExtension(filePath);
+                Log.Info($"{protoName} proto ==> golang");
                 var command = Settings.Protoc_Path + string.Format(" -I={0} --go_out={1} {2}", protoPath, outFolder, filePath);
                 var log = Common.Cmd(command);
             }
@@ -79,6 +96,8 @@ namespace HiProtobuf.Lib
             for (int i = 0; i < files.Length; i++)
             {
                 var filePath = files[i];
+                string protoName = Path.GetFileNameWithoutExtension(filePath);
+                Log.Info($"{protoName} proto ==> java");
                 var command = Settings.Protoc_Path + string.Format(" -I={0} --java_out={1} {2}", protoPath, outFolder, filePath);
                 var log = Common.Cmd(command);
             }
@@ -93,6 +112,8 @@ namespace HiProtobuf.Lib
             for (int i = 0; i < files.Length; i++)
             {
                 var filePath = files[i];
+                string protoName = Path.GetFileNameWithoutExtension(filePath);
+                Log.Info($"{protoName} proto ==> python");
                 var command = Settings.Protoc_Path + string.Format(" -I={0} --python_out={1} {2}", protoPath, outFolder, filePath);
                 var log = Common.Cmd(command);
             }
